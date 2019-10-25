b46086ce08d1f5ec53ee121b6b29e5004ecf1a74
Loewy Malkovich <loewymalkovich@Loewys-MacBook-Pro.local>
2019-10-25 14:42:21 -0700

    test run project, fix bugs with treat views

--- PierresTreats/Models/PierresTreatsContext.cs
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -5,7 +5,7 @@ namespace PierresTreats.Models
 {
   public class PierresTreatsContext : IdentityDbContext<ApplicationUser>
   {
-    public virtual DbSet<Flavor> Flavor { get; set; }
+    public virtual DbSet<Flavor> Flavors { get; set; }
     public DbSet<Treat> Treats { get; set; }
     public DbSet<FlavorTreat> FlavorTreat { get; set; }
 
